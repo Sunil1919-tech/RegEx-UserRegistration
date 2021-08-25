65f638e7ece5f59df289501865478810645dfae2
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-25 16:27:07 +0530

    Added: Welcome to User Registration Program using RegEx

---
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+/**
+ * purpose - To implement Regex method for User Registration
+ * @author  Sunil
+ * @version 16.0
+ * @since 23/08/2021
+ */
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Registration System");
+
+    }
+
+}
+
